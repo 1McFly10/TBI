80906e07fb3035b0f3680a089fba985748ffdee0
1McFly10 <vlados3702@gmail.com>
2021-02-03 11:31:18 +0200

    добавил фон под What We Do?, пофиксил переход по ссылкам в шапке

--- scripts/header.js
+++ scripts/header.js
@@ -16,9 +16,9 @@ $('.cancel-btn').on('click', function(e) {
 });
 
 // $('.codrops-demos_sidebar a').on('click', (e)=>{
-//     // e.preventDefault();
 //     $('.left_sidebar').toggleClass('menu_active');
 //     $('.main_content').toggleClass('content_active');
+//     $('.left_sidebar').css('overflowX','inherit')
 //     setTimeout(()=>{
 //         $('.menu-btn').css("display","flex");
 //     }, 280)
